a30051838eb60f20e149b194c9ed2b1918d7e7a1
Kevin C. Zhang <108235966+kevinz1194@users.noreply.github.com>
2024-04-16 10:24:12 -0400

    Update app.R

--- app.R
+++ app.R
@@ -90,59 +90,59 @@ server <- function(input, output) {
     
     response <- predict(model_final, df, type='response')
     
-    if (response < 0.0002414687) {score <- 1} 
-    else if (response < 0.0019252998) {score <- 2} 
-    else if (response < 0.0023406122) {score <- 3} 
-    else if (response < 0.0026053226) {score <- 4} 
-    else if (response < 0.0028629330) {score <- 5} 
-    else if (response < 0.0030822307) {score <- 6} 
-    else if (response < 0.0033125030) {score <- 7} 
-    else if (response < 0.0035052083) {score <- 8} 
-    else if (response < 0.0036847276) {score <- 9} 
-    else if (response < 0.0038788431) {score <- 10} 
+         if (response <= 0.001264987) {score <- 1} 
+    else if (response <= 0.001512010) {score <- 2} 
+    else if (response <= 0.001730142) {score <- 3} 
+    else if (response <= 0.001915465) {score <- 4} 
+    else if (response <= 0.002077231) {score <- 5} 
+    else if (response <= 0.002265669) {score <- 6} 
+    else if (response <= 0.002460944) {score <- 7} 
+    else if (response <= 0.002656456) {score <- 8} 
+    else if (response <= 0.002870943) {score <- 9} 
+    else if (response <= 0.003053774) {score <- 10} 
     
-    else if (response < 0.0040652926) {score <- 11} 
-    else if (response < 0.0042330307) {score <- 12} 
-    else if (response < 0.0044408084) {score <- 13} 
-    else if (response < 0.0046558164) {score <- 14} 
-    else if (response < 0.0048401899) {score <- 15} 
-    else if (response < 0.0050318944) {score <- 16} 
-    else if (response < 0.0052317505) {score <- 17} 
-    else if (response < 0.0054104743) {score <- 18} 
-    else if (response < 0.0055949239) {score <- 19} 
-    else if (response < 0.0058052005) {score <- 20} 
+    else if (response <= 0.003265650) {score <- 11} 
+    else if (response <= 0.003480987) {score <- 12} 
+    else if (response <= 0.003768343) {score <- 13} 
+    else if (response <= 0.004031062) {score <- 14} 
+    else if (response <= 0.004282552) {score <- 15} 
+    else if (response <= 0.004547730) {score <- 16} 
+    else if (response <= 0.004810713) {score <- 17} 
+    else if (response <= 0.005049220) {score <- 18} 
+    else if (response <= 0.005360812) {score <- 19} 
+    else if (response <= 0.005642311) {score <- 20} 
     
-    else if (response < 0.0060102816) {score <- 21} 
-    else if (response < 0.0061999520) {score <- 22} 
-    else if (response < 0.0064498442) {score <- 23} 
-    else if (response < 0.0067305407) {score <- 24} 
-    else if (response < 0.0069936946) {score <- 25} 
-    else if (response < 0.0072599684) {score <- 26} 
-    else if (response < 0.0075419377) {score <- 27} 
-    else if (response < 0.0078242595) {score <- 28} 
-    else if (response < 0.0081003418) {score <- 29} 
-    else if (response < 0.0084233431) {score <- 30} 
+    else if (response <= 0.005912152) {score <- 21} 
+    else if (response <= 0.006187035) {score <- 22} 
+    else if (response <= 0.006482131) {score <- 23} 
+    else if (response <= 0.006795176) {score <- 24} 
+    else if (response <= 0.007096159) {score <- 25} 
+    else if (response <= 0.007475686) {score <- 26} 
+    else if (response <= 0.007820915) {score <- 27} 
+    else if (response <= 0.008191861) {score <- 28} 
+    else if (response <= 0.008585006) {score <- 29} 
+    else if (response <= 0.008929379) {score <- 30} 
     
-    else if (response < 0.0087711468) {score <- 31} 
-    else if (response < 0.0091478702) {score <- 32} 
-    else if (response < 0.0095864050) {score <- 33} 
-    else if (response < 0.0101254803) {score <- 34} 
-    else if (response < 0.0106085799) {score <- 35} 
-    else if (response < 0.0111708917) {score <- 36} 
-    else if (response < 0.0118524883) {score <- 37} 
-    else if (response < 0.0125295036) {score <- 38} 
-    else if (response < 0.0134169667) {score <- 39} 
-    else if (response < 0.0143876027) {score <- 40} 
+    else if (response <= 0.009364284) {score <- 31} 
+    else if (response <= 0.009784004) {score <- 32} 
+    else if (response <= 0.010321594) {score <- 33} 
+    else if (response <= 0.010914968) {score <- 34} 
+    else if (response <= 0.011564956) {score <- 35} 
+    else if (response <= 0.012269121) {score <- 36} 
+    else if (response <= 0.013089024) {score <- 37} 
+    else if (response <= 0.013902984) {score <- 38} 
+    else if (response <= 0.014925043) {score <- 39} 
+    else if (response <= 0.016103152) {score <- 40} 
     
-    else if (response < 0.0155228361) {score <- 41} 
-    else if (response < 0.0168976873) {score <- 42} 
-    else if (response < 0.0182511003) {score <- 43} 
-    else if (response < 0.0199890044) {score <- 44} 
-    else if (response < 0.0223121048) {score <- 45} 
-    else if (response < 0.0256560892) {score <- 46} 
-    else if (response < 0.0293034912) {score <- 47} 
-    else if (response < 0.0346857267) {score <- 48} 
-    else if (response < 0.0439990588) {score <- 49} 
+    else if (response <= 0.017557660) {score <- 41} 
+    else if (response <= 0.018975357) {score <- 42} 
+    else if (response <= 0.020854602) {score <- 43} 
+    else if (response <= 0.023320539) {score <- 44} 
+    else if (response <= 0.026778868) {score <- 45} 
+    else if (response <= 0.030349420) {score <- 46} 
+    else if (response <= 0.035739083) {score <- 47} 
+    else if (response <= 0.045293501) {score <- 48} 
+    else if (response <= 0.063164834) {score <- 49} 
     else {score <- 50}
     
     score <- score - 1
